03bb0eafcb690daf44d979083b840b0313e8c7ad
space.data.robert@gmail.com <space.data.robert@gmail.com>
2022-12-21 19:10:22 +0900

    fix. 모델 파라미터 범위 변경

--- source/regressor/XGBRegressor.py
+++ source/regressor/XGBRegressor.py
@@ -11,8 +11,7 @@ def xgbregressor():
 
 def xgbregressor_params():
     prefix = 'xgbregressor'
-
-    return {
+    params = {
         f'{prefix}__n_estimators': optuna.distributions.IntDistribution(1000, 10000),
         f'{prefix}__reg_alpha': optuna.distributions.LogUniformDistribution(1e-8, 100.0),
         f'{prefix}__reg_lambda': optuna.distributions.LogUniformDistribution(1e-8, 100.0),
@@ -21,3 +20,4 @@ def xgbregressor_params():
         f'{prefix}__max_depth': optuna.distributions.IntDistribution(2, 9),
         f'{prefix}__colsample_bytree': optuna.distributions.FloatDistribution(0.1, 1.0),
     }
+    return params
